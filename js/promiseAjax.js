61d94a0d199fb563af3c4014b590cc9faf062dd2
HaibiPeng <haibi.peng@aalto.fi>
2021-05-06 17:15:35 +0800

    latest update

--- js/promiseAjax.js
+++ js/promiseAjax.js
@@ -54,4 +54,67 @@ function postJSON(url, data) {
     })
 }
 
+//封装axios
+var Axios = {
+    get: getJSON(url),
+    post: postJSON(url, data)
+}
+
+//或者定义Axios类
+class Axios {
+    constructor() {
+
+    }
+
+    get(url) {
+        return new Promise((resolve, reject) => {
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', url + '?' + params.join('&'), true);
+
+            xhr.onreadystatechange = () => {
+                //交互完成
+                if (xhr.readyState === 4) {
+                    if (xhr.status >= 200 && xhr.status < 400) {
+                        try {
+                            var data = JSON.parse(xhr.responseText);
+                            resolve(data);
+                        } catch (e) {
+                            reject(e);
+                        }
+                    } else {
+                        reject(new Error("Request failed: " + xhr.responseText));
+                    }
+                }
+            }
+            xhr.send();
+        })
+    }
+
+    post(url, data) {
+        return new Promise((resolve, reject) => {
+            var xhr = new XMLHttpRequest();
+            xhr.open('POST', url, true);
+            //设置请求头content-type
+            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+            xhr.onreadystatechange = () => {
+                if (xhr.readyState === 4) {
+                    if (xhr.status >= 200 && xhr.status < 400) {
+                        try {
+                            resolve(JSON.parse(xhr.responseText));
+                        } catch (e) {
+                            reject(e);
+                        }
+                    } else {
+                        reject(new Error("Request failed: " + xhr.responseText));
+                    }
+                }
+            }
+            xhr.send(JSON.stringify(data));
+        })
+    }
+}
+
+let axios = new Axios();
+
 getJSON(url).then(res => console.log(res));
