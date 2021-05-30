70fac30263cd7bd0e64f6198e65e5d1f8b35ea42
HaibiPeng <penghaibi@kuaishou.com>
2021-05-30 17:37:29 +0800

    recent update

---
+++ Intro-to-Vue-3/components/ProductDisplay.js
@@ -0,0 +1,108 @@
+app.component('product-display', {
+  props: {
+    premium: {
+      type: Boolean,
+      required: true
+    }
+  },
+  template: 
+  /*html*/
+  `<div class="product-display">
+    <div class="product-container">
+      <div class="product-image">
+        <img v-bind:src="image">
+      </div>
+      <div class="product-info">
+        <h1>{{ title }}</h1>
+
+        <p v-if="inStock">In Stock</p>
+        <p v-else>Out of Stock</p>
+
+        <p>Shipping: {{ shipping }}</p>
+        <ul>
+          <li v-for="detail in details">{{ detail }}</li>
+        </ul>
+
+        <div 
+          v-for="(variant, index) in variants" 
+          :key="variant.id" 
+          @mouseover="updateVariant(index)" 
+          class="color-circle" 
+          :style="{ backgroundColor: variant.color }">
+        </div>
+        
+        <button 
+          class="button" 
+          :class="{ disabledButton: !inStock }" 
+          :disabled="!inStock" 
+          v-on:click="addToCart">
+          Add to Cart
+        </button>
+
+        <button 
+          class="button" 
+          v-on:click="changeTitle">
+          Change Title
+        </button>
+      </div>
+    </div>
+    <review-list v-if="reviews.length" :reviews="reviews"></review-list>
+    <review-form @review-submitted="addReview"></review-form>
+  </div>`,
+  data() {
+    return {
+        product: 'Socks',
+        brand: 'Vue Mastery',
+        title: 'Vue Mastery Socks',
+        products: ['Coat', 'Socks', 'Shirts', 'Pants'],
+        brands: ['Nike', 'Adidas', 'Puma', 'Air Jordan'],
+        selectedVariant: 0,
+        details: ['50% cotton', '30% wool', '20% polyester'],
+        variants: [
+          { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+          { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
+        ],
+        reviews: []
+    }
+  },
+  methods: {
+      addToCart() {
+          this.$emit('add-to-cart', this.variants[this.selectedVariant].id)
+      },
+      updateVariant(index) {
+          this.selectedVariant = index
+      },
+      addReview(review) {
+        this.reviews.push(review)
+      },
+      changeTitle() {
+        this.product = this.products[Math.floor(Math.random() * 4)];
+        this.brand = this.brands[Math.floor(Math.random() * 4)];
+      }
+  },
+  computed: {
+      // title() {
+      //     return this.brand + ' ' + this.product
+      // },
+      image() {
+          return this.variants[this.selectedVariant].image
+      },
+      inStock() {
+          return this.variants[this.selectedVariant].quantity
+      },
+      shipping() {
+        if (this.premium) {
+          return 'Free'
+        }
+        return 2.99
+      }
+  },
+  watch: {
+    brand(val) {
+      this.title = val + ' ' + this.product;
+    },
+    product(val) {
+      this.title = this.brand + ' ' + val;
+    }
+  }
+})
