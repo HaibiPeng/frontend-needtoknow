1ed5538ea27d743beeba8741622ba2db12f7d132
HaibiPeng <penghaibi@kuaishou.com>
2021-06-01 14:18:16 +0800

    vuex-todo-app

---
+++ vuex-todo/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <AddTodos />
+      <FilterTodos />
+      <Todos />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Todos from "./components/Todos.vue";
+import AddTodos from "./components/AddTodo.vue";
+import FilterTodos from "./components/FilterTodos.vue";
+
+@Component({
+  components: {
+    Todos,
+    AddTodos,
+    FilterTodos,
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style lang="less">
+body {
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+  line-height: 1.6;
+  background: #e8f7f0;
+}
+.container {
+  max-width: 1100px;
+  margin: auto;
+  overflow: auto;
+  padding: 0 2rem;
+}
+</style>
---
+++ vuex-todo/src/components/AddTodo.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <h3>Add Todo</h3>
+    <div class="add">
+      <form @submit="onSubmit">
+        <input type="text" v-model="title" placeholder="Add Todo..." />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Component, Vue } from "vue-property-decorator";
+import { mapActions } from "vuex";
+
+@Component({
+  name: "AddTodo",
+  data() {
+    return {
+      title: "",
+    };
+  },
+  methods: {
+    ...mapActions(["addTodo"]),
+    onSubmit(e) {
+      e.preventDefault();
+      const todo = {
+        title: (this as any).title,
+        //id: (this as any).$store.state.todos.todos[0].id,
+      };
+      (this as any).addTodo(todo);
+    },
+  },
+})
+export default class AddTodos extends Vue {}
+</script>
+
+<style scoped>
+form {
+  display: flex;
+}
+
+input[type="text"] {
+  flex: 10;
+  padding: 10px;
+  border: 1px solid #41b883;
+  outline: 0;
+}
+
+input[type="submit"] {
+  flex: 2;
+  background: #41b883;
+  color: #fff;
+  border: 1px #41b883 solid;
+  cursor: pointer;
+}
+</style>
